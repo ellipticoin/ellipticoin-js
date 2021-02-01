877531beb77dd08856e4ed64d31e5ee645ead3b4
Mason Fischer <mason@kissr.co>
2021-02-01 09:10:50 -0500

    wip

--- src/constants.js
+++ src/constants.js
@@ -7,12 +7,39 @@ const LAST_BLOCK_OF_FIRST_ERA =
   BLOCKS_PER_ERA * FIRST_ERA_ISSUANCE_PER_BLOCK -
   PROTOTYPE_ISSUANCE / FIRST_ERA_ISSUANCE_PER_BLOCK;
 const DEFAULT_FEE = 3000n;
+const ELC = Buffer.from(
+    "0000000000000000000000000000000000000001",
+    "hex"
+  ).toString("base64")
+const ETH = Buffer.from(
+    "eb4c2781e4eba804ce9a9803c67d0893436bb27d",
+    "hex"
+  ).toString("base64")
+const BTC = Buffer.from(
+    "6b175474e89094c44da98b954eedeac495271d0f",
+    "hex"
+  ).toString("base64")
+const USD = Buffer.from(
+    "6b175474e89094c44da98b954eedeac495271d0f",
+    "hex"
+  ).toString("base64")
+const TICKERS = {
+  [ELC]: "ELC",
+  [ETH]: "ETH",
+  [BTC]: "BTC",
+  [USD]: "USD",
+};
 
 module.exports = {
-  BLOCKS_PER_ERA,
-  LAST_BLOCK_OF_FIRST_ERA,
-  NUMBER_OF_ERAS,
+    BTC,
+    ELC,
+    ETH,
   BASE_FACTOR,
+  BLOCKS_PER_ERA,
   DEFAULT_FEE,
   FIRST_ERA_ISSUANCE_PER_BLOCK,
+  LAST_BLOCK_OF_FIRST_ERA,
+  NUMBER_OF_ERAS,
+  TICKERS,
+USD,
 };
