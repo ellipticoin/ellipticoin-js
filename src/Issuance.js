cc5436bea559ba1120911b27d3fb8d20a895eeb6
Mason Fischer <mason@kissr.co>
2021-02-26 08:39:02 -0500

    Convert exponents from BigInts to Numbers

--- src/Issuance.js
+++ src/Issuance.js
@@ -1,10 +1,10 @@
 const { BLOCKS_PER_ERA, BASE_FACTOR, BTC, ETH } = require("./constants");
 export function blockReward(blockNumber, token) {
   if (blockNumber === null) return 0n;
-  const era = blockNumber / BLOCKS_PER_ERA;
+  const era = Math.round(Number(blockNumber) / Number(BLOCKS_PER_ERA));
 
   if ([BTC, ETH].includes(token)) {
-    return (BASE_FACTOR * 128n * 10n ** 6n) / 2n ** era / 10n ** 8n / 2n;
+    return (BASE_FACTOR * 128n * 10n ** 6) / 2n ** era / 10n ** 8 / 2n;
   } else {
     return 0n;
   }
